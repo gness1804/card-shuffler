b0bc9821b551463e8fbdc957365a255ce47362e0
gness1804 <graham.nessler@gmail.com>
2021-07-14 19:51:10 -0500

    add prettier

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Suit } from "../../models/card";
+import { Suit } from '../../models/card';
 
 interface Props {
   suit: Suit;
   rank: string;
 }
 
-const Card = ({suit, rank}: Props) => {
+const Card = ({ suit, rank }: Props) => {
   return (
     <div>
       <p>{rank}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
