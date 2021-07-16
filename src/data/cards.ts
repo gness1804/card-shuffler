f89b283d6bd2bbc913de8c6a9fe638bcfbcb2165
gness1804 <graham.nessler@gmail.com>
2021-07-15 19:35:17 -0500

    add missing 10s to cards

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -6,7 +6,21 @@ import { v4 } from 'uuid';
 const createData = (): CardType[] => {
   const res = [];
   const suits = [Suit.S, Suit.C, Suit.D, Suit.H];
-  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+  const ranks = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
 
   for (const suit of suits) {
     for (const rank of ranks) {
--- src/test/cards.test.ts
+++ src/test/cards.test.ts
@@ -1,8 +1,8 @@
 import { cards } from '../data/cards';
 
 describe('cards', () => {
-  it('should return 48 cards', () => {
-    expect(cards.length).toBe(48);
+  it('should return 52 cards', () => {
+    expect(cards.length).toBe(52);
   });
 
   it('cards should not contain duplicates', () => {
